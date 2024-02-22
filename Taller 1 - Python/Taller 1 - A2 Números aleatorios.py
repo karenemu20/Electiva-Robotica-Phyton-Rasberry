a5550677628adb5f108cb1881f047689d278dfa1
karenemu20 <158532259+karenemu20@users.noreply.github.com>
2024-02-22 00:07:51 -0500

    Add files via upload

---
+++ Taller 1 - Python/Taller 1 - A2 Números aleatorios.py
@@ -0,0 +1,10 @@
+#Taller 1- A2 NÚMEROS ALEATORIOS
+import random
+def generador_de_numeros(min, max, num):
+    return [random.randint(min, max) for _ in range(num)]
+print("GENERADOR DE NÚMEROS ALEATORIOS\nIngrese un rango de números")
+min = int(input('Desde el número: \n'))
+max = int(input('Hasta el número: \n'))
+num = int(input('Ingrese la cantidad de números aleatorias a generar dentro de ese rango: \n'))
+lst = generador_de_numeros(min,max,num)
+print('Los números aleatorios generados entre', min, "y", max, "son:", lst, '\n')
