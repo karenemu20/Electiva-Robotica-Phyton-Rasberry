336bcb9c7194ec138ff9287b6da2beb33dfdf733
karenemu20 <158532259+karenemu20@users.noreply.github.com>
2024-02-29 15:52:51 -0500

    Update B3 - Volumenes.py

--- Taller 1 - Python/B3 - Volumenes.py
+++ Taller 1 - Python/B3 - Volumenes.py
@@ -25,29 +25,29 @@ def main():
         
     opcion = int(input("Seleccione una opción: "))
         
-    if opcion == 1:
-            base = float(input("Ingrese la base del prisma (cm): "))
-            altura = float(input("Ingrese la altura del prisma (cm): "))
-            resultado = volumen_prisma(base, altura)
-            print("El volumen del prisma es:", resultado, 'cm3')
-    elif opcion == 2:
-            base = float(input("Ingrese la base de la pirámide (cm): "))
-            altura = float(input("Ingrese la altura de la pirámide (cm): "))
-            resultado = volumen_piramide(base, altura)
-            print("El volumen de la pirámide es:", resultado,'cm3')
-    elif opcion == 3:
-            radio = float(input("Ingrese el radio del cono (cm):"))
-            altura = float(input("Ingrese la altura del cono(cm): "))
-            resultado = volumen_cono(radio, altura)
-            print("El volumen del cono es:", resultado,'cm3')
-    elif opcion == 4:
-            radio = float(input("Ingrese el radio del cilindro (cm): "))
-            altura = float(input("Ingrese la altura del cilindro (cm): "))
-            resultado = volumen_cilindro(radio, altura)
-            print("El volumen del cilindro es:", resultado,'cm3')
-    elif opcion == 5:
-            print("Programa finalizado")
-            valor = 5
+     if opcion == 5:
+            break  # Salir del bucle si elige la opción 5
+     if 1 <= opcion <= 4:
+            if opcion == 1:
+                base = float(input("Ingrese la base del prisma: "))
+                altura = float(input("Ingrese la altura del prisma: "))
+                resultado = volumen_prisma(base, altura)
+                print("El volumen del prisma es:", resultado)
+            elif opcion == 2:
+                base = float(input("Ingrese la base de la pirámide: "))
+                altura = float(input("Ingrese la altura de la pirámide: "))
+                resultado = volumen_piramide(base, altura)
+                print("El volumen de la pirámide es:", resultado)
+            elif opcion == 3:
+                radio = float(input("Ingrese el radio del cono: "))
+                altura = float(input("Ingrese la altura del cono: "))
+                resultado = volumen_cono(radio, altura)
+                print("El volumen del cono es:", resultado)
+            elif opcion == 4:
+                radio = float(input("Ingrese el radio del cilindro: "))
+                altura = float(input("Ingrese la altura del cilindro: "))
+                resultado = volumen_cilindro(radio, altura)
+                print("El volumen del cilindro es:", resultado)
     else:
             print("Opción no válida. Por favor, seleccione un número del 1 al 5.")
     return valor
