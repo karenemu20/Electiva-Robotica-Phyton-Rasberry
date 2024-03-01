5ec8e8b51f05ae1a14f1289772a50bcf46275d46
karenemu20 <158532259+karenemu20@users.noreply.github.com>
2024-02-29 23:46:36 -0500

    Update C5 - Plot 2D.py

--- Taller 1 - Python/C5 - Plot 2D.py
+++ Taller 1 - Python/C5 - Plot 2D.py
@@ -1,27 +1,31 @@
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy import signal
 
-# Función para dibujar el nombre 'KAREN'
+# KAREN
 def dibujar_nombre_karen():
-    plt.text(0.1, 0.5, 'K', fontsize=100, color='blue')
-    plt.plot([0.2, 0.2], [0.3, 0.7], color='blue')
-    plt.plot([0.2, 0.4], [0.7, 0.3], color='blue')
-    plt.plot([0.4, 0.4], [0.3, 0.7], color='blue')
+    plt.text(0.0, 0.5, 'K', fontsize=100, color='blue')
+    plt.text(0.1, 0.5, 'A', fontsize=100, color='blue')
+    plt.text(0.2, 0.5, 'R', fontsize=100, color='blue')
+    plt.text(0.3, 0.5, 'E', fontsize=100, color='blue')
+    plt.text(0.4, 0.5, 'N', fontsize=100, color='blue')
 
-# Función para dibujar el nombre 'DIANA'
+# DIANA
 def dibujar_nombre_diana():
-    plt.text(0.6, 0.5, 'D', fontsize=100, color='green')
-    plt.plot([0.7, 0.7], [0.3, 0.7], color='green')
-    plt.plot([0.7, 0.8], [0.3, 0.3], color='green')
-    plt.plot([0.8, 0.8], [0.3, 0.7], color='green')
-    plt.plot([0.8, 0.7], [0.5, 0.5], color='green')
+    plt.text(0.55, 0.5, '&', fontsize=100, color='magenta')
+    plt.text(0.7, 0.5, 'D', fontsize=100, color='green')
+    plt.text(0.82, 0.5, 'I', fontsize=100, color='green')
+    plt.text(0.86, 0.5, 'A', fontsize=100, color='green')
+    plt.text(0.97, 0.5, 'N', fontsize=100, color='green') 
+    plt.text(1.1, 0.5, 'A', fontsize=100, color='green')
 
-# Configuración del plot
-plt.figure(figsize=(8, 4))
+# plot
+plt.figure(figsize=(10, 4))
 plt.axis('off')  # Ocultar los ejes
 
-# Dibujar los nombres
+# Dibujar
 dibujar_nombre_karen()
 dibujar_nombre_diana()
 
-# Mostrar el plot
+# Mostrar el plot
 plt.show()
